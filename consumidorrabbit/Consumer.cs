0d9200550e62d956182bbdbe1e38fab38f9bdb4b
Rodrigo Ferreira <rodrigo.milioli@lojasrede.com.br>
2021-04-06 09:02:35 -0300

    initial commit rodrigo
    initial commit rodrigo

---
+++ consumidorrabbit/Consumer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace consumidorrabbit
+{
+    public class Consumer
+    {
+        public bool Consumidor()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.Password = "guest";
+            factory.Port = 7070;
+            factory.UserName = "guest";
+
+            var message = new MessageTransport();
+            message.guid = Guid.NewGuid();
+            message.message = "Esta é uma nova mensagem! Olá mundo";
+            message.define = 0;
+            message.nomeleitura = "meuservico.com/rota1";
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue:"MessageMeuServico", false, false, false, null);
+            var menssagem = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(menssagem);
+            channel.BasicPublish(exchange:"", routingKey: "MessageMeuServico", basicProperties: null, body: body);
+            return true;
+        }
+    }
+}
---
+++ consumidorrabbit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consumidorrabbit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dados = new Consumer().Consumidor();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
